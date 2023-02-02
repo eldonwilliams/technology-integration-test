f55b9301f6859207d0937650909865e6e6f8db10
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-02 05:53:56 -0600

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,14 @@
-console.log("[Server] Hello, world!");
+console.time("startup");
+console.timeLog("startup", "Server is now starting...");
+
+import express from "express";
+
+const app = express();
+const REST_PORT: number = parseInt(process.env.REST_PORT ?? "8000");
+const SOCKET_PORT: number = parseInt(process.env.SOCKET_PORT ?? "7979");
+console.timeLog("startup", `Socketio unimplemented yet, port would be: ${SOCKET_PORT}`)
+
+console.timeLog("startup", `Rest server is trying to listen on port: ${REST_PORT}`);
+app.listen(REST_PORT ?? 8000, () => {
+
+});
