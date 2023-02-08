4f42a1e941695b3fa382aa03056678db9aaab70e
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-07 19:59:37 -0600

    :construction: uses getEnv and ends timer
    Just moving files to my main PC lol

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,13 +2,14 @@ console.time("startup");
 console.timeLog("startup", "Server is now starting...");
 
 import express from "express";
+import { getEnvNumber } from "./util/getEnv";
 
 const app = express();
-const REST_PORT: number = parseInt(process.env.REST_PORT ?? "8000");
-const SOCKET_PORT: number = parseInt(process.env.SOCKET_PORT ?? "7979");
+const REST_PORT = getEnvNumber("REST_PORT", 8000);
+const SOCKET_PORT = getEnvNumber("SOCKET_PORT", 7979);
 console.timeLog("startup", `Socketio unimplemented yet, port would be: ${SOCKET_PORT}`)
 
 console.timeLog("startup", `Rest server is trying to listen on port: ${REST_PORT}`);
-app.listen(REST_PORT ?? 8000, () => {
-
+app.listen(REST_PORT, () => {
+    console.timeEnd("startup")
 });
