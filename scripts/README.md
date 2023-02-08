88daa9ec590925ee8fe3746721d699d16b4fce4d
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-08 08:12:33 -0600

    :hammer: Just a few for now

---
+++ scripts/README.md
@@ -0,0 +1,4 @@
+## What is this?
+
+These are helper scripts that automate some annoying tasks.
+What each script does can be found in the header comment of each file.
---
+++ scripts/launch.sh
@@ -0,0 +1,5 @@
+# This script is how you can start the app.
+# The --build option makes sure the newest version is built.
+
+echo "Starting Farm IDLE"
+docker compose --build
