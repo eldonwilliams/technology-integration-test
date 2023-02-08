ccc77cdd9362038f44b2d94b73754e48f44b869b
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-08 11:40:34 -0600

    :white_check_mark: generate test reports #1
    Is there a way to do this automatically with only docker? I tried but I need it in the jest-reports directory because I only want that one directory bound to the host OS.
    
    Idk, not that big of a deal.

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,25 @@ console.time("startup");
 console.timeLog("startup", "Server is now starting...");
 
 import express from "express";
-import { getEnvNumber } from "./util/getEnv";
+import { getEnvNumber, getEnvString } from "./util/getEnv";
+import {copyFile, rm as deleteFile} from "fs";
+import {join} from "path";
 
 const app = express();
 const REST_PORT = getEnvNumber("REST_PORT", 8000);
 const SOCKET_PORT = getEnvNumber("SOCKET_PORT", 7979);
 console.timeLog("startup", `Socketio unimplemented yet, port would be: ${SOCKET_PORT}`)
+
+let shouldGen = getEnvString("GEN_TEST_REPORTS", "true")?.toLowerCase() === "true";
+if (shouldGen) {
+    let reportSrc = join(__dirname, "../jest.report.txt")
+    let reportDest = join(__dirname, "../jest-reports/jest.report.txt")
+    console.log("GEN_TEST_REPORTS was true")
+    copyFile(reportSrc, reportDest, (err) => {
+        if (err) return console.log(`${err} <- Something went wrong whilst saving`);
+        deleteFile(reportSrc, ()=>{});
+    });
+}
 
 console.timeLog("startup", `Rest server is trying to listen on port: ${REST_PORT}`);
 app.listen(REST_PORT, () => {
