8e9182324dde4d3540844b71bf1c51e1fb05566f
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-10 08:13:32 -0600

    :fire: Removed some testing scripts

--- server/src/routes/array.ts
+++
@@ -1,8 +0,0 @@
-export default [
-    () => {
-        console.log("Hello from array.ts (or js i guess)")
-    },
-    () => {
-        console.log("Also, hello from the other thing in array.ts")
-    }
-]
