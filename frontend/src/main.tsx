a4ae19512eb89ad2b6fb19a4e0b51cc88090abd8
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-11 14:27:26 -0600

    :construction: implemented router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './components/App'
+import Error from './components/Error'
+import Signup from './components/Signup'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/signup",
+        element: <Signup />,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
