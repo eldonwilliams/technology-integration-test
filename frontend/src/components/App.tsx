21da40db882ac094f5e2b888edf6aa57d06642de
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-11 14:29:23 -0600

    :children_crossing: added offline and links

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
+import { Link, Outlet, useOutlet } from 'react-router-dom'
+import useOnline from '../hooks/useOnline';
 import './App.css'
-import Signup from './Signup'
 
 function App() {
+  const outlet = useOutlet();
+  const online = useOnline();
+
   return (
     <div className="App">
+      {!online && <h1>Offline.</h1>}
       <h1>Farm IDLE</h1>
       <div className="card">
-        <Signup />
+        {outlet ?? <div className="links">
+          <Link to="/signup">Sign-up</Link>
+          <Link to="/login">Login</Link>
+        </div>}
         <p>
           Server Contact Test.
         </p>
