37bdc2c0bf02dcae69a27f9941726faee52b7267
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-11 13:14:54 -0600

    :fire: setup for work

---
+++ frontend/src/Signup.tsx
@@ -0,0 +1,15 @@
+
+
+function Signup() {
+    return (
+        <>
+            <form>
+                <input type="text" />
+                <input type="password" />
+            </form>
+            <button>Sign-up</button>
+        </>
+    )
+}
+
+export default Signup;
