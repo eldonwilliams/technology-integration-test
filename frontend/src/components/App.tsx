43b8ea614c7b3a4857adae533c75736567a5cbd4
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-13 14:18:48 -0600

    :sparkles: add redux & auth stuff

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,16 +1,29 @@
-import { Link, Outlet, useOutlet } from 'react-router-dom'
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useOutlet } from 'react-router-dom'
+import useAuthUpdate from '../hooks/useAuthUpdate';
 import useOnline from '../hooks/useOnline';
-import './App.css'
+import { RootState } from '../Store';
+import './App.css';
+import Logout from './Logout';
 
 function App() {
+  const auth = useSelector((state: RootState) => state.auth);
   const outlet = useOutlet();
   const online = useOnline();
+  const authUpdate = useAuthUpdate();
+
+  useEffect(() => {
+    authUpdate();
+  }, []);
 
   return (
     <div className="App">
       {!online && <h1>Offline.</h1>}
       <h1>Farm IDLE</h1>
+      {auth.authenticated && <p>Hello, {auth.whois}.</p>}
       <div className="card">
+        {auth.authenticated && <Logout />}
         {outlet ?? <div className="links">
           <Link to="/signup">Sign-up</Link>
           <Link to="/login">Login</Link>
