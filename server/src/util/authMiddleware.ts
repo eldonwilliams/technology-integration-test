703f8b55e95d89434b96e861b48bfb0265d2ff45
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-16 11:28:48 -0600

    🏗️ redid for tsoa and moved loggedOutMiddleware here

--- server/src/util/authMiddleware.ts
+++ server/src/util/authMiddleware.ts
@@ -2,22 +2,41 @@ import { Handler } from "express";
 import { getEnvString } from "./getEnv";
 import { randomBytes } from "crypto";
 import { verify } from "jsonwebtoken";
+import * as express from "express";
 
 const randBytes = randomBytes(128).toString("hex");
 const JWT_KEY = getEnvString("JWT_KEY", randBytes) as string;
 if (JWT_KEY === randBytes) console.warn("JWT_KEY is unset! All and data encrypted with the randBytes will be unrecoverable once restarted.")
 
-let authMiddleware: Handler = (req, res, next) => {
-    if (req.auth) return next();
-    const sessionToken = req.cookies.sessionToken;
-    if (sessionToken === undefined) return next();
-    verify(sessionToken, JWT_KEY, {}, (err, decoded) => {
-        if (err !== null || decoded === undefined || typeof decoded === "string") return next();
-        req.authed = true;
-        req.auth = decoded.user;
-        next();
+export function expressAuthentication(request: express.Request, securityName: string, scopes: string[]): Promise<any> {
+    if (securityName !== "sessionToken") return Promise.reject({});
+    const sessionToken = request.cookies.sessionToken;
+    if (sessionToken === undefined) return Promise.resolve({
+        authed: false,
+        auth: undefined,
     });
-};
+    return new Promise((resolve, reject) => {
+        verify(sessionToken, JWT_KEY, {}, (err, decoded) => {
+            if (err !== null || decoded === undefined || typeof decoded === "string") return reject({});
+            resolve({
+                authed: true,
+                auth: decoded.user,
+            });
+        });
+    });
+}
 
-export default authMiddleware;
+export interface LoggedInErrResponse {
+    err: "Loggedin",
+}
+
+/**
+ * A middleware that makes sure the client is NOT logged in.
+ */
+export const loggedOutMiddleware: Handler = (req, res, next) => {
+    if (req.authed) return res.status(400).send({ err: "LoggedIn", });
+    next();
+}
+
+export default expressAuthentication;
 export { JWT_KEY, }
