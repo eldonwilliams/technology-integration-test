d17a7a840be12a4b6ba8e5d302df03239aab682a
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-16 13:26:21 -0600

    :memo: add coments

--- server/src/controllers/statusController.ts
+++ server/src/controllers/statusController.ts
@@ -1,9 +1,12 @@
 import * as express from "express";
-import { Controller, Get, Request, Route, Security, Tags, } from "tsoa";
+import { Controller, Get, Request, Route, Security, SuccessResponse, Tags, } from "tsoa";
 import { GetStatusResponse } from "../services/statusService";
 
 @Route("/status")
 export class StatusController extends Controller {
+    /**
+     * Checks the status of the server and additionally checks if you are authenticated. Security is optional.
+     */
     @Security('sessionToken', ["optional?"])
     @Get()
     @Tags("Status")
