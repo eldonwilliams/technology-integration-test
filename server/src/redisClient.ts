4b344b267332cc1fd21dff2ae4427c7b45e341f9
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-17 09:56:44 -0600

    🧑‍💻 added error type and const

--- server/src/redisClient.ts
+++ server/src/redisClient.ts
@@ -1,6 +1,16 @@
 import { createClient } from "redis";
 import { getEnvString } from "./util/getEnv";
 
+/**
+ * A Error that occurs because of Redis, should have status of 500.
+ * Also see the RedisError constant.
+ */
+export interface RedisError {
+    err: "Redis Failure"
+}
+
+export const RedisError: RedisError = { err: "Redis Failure", }
+
 const REDIS_URL = getEnvString("REDIS_URL", "redis://redis:6379") as string;
 
 export const redisClient = createClient({ url: REDIS_URL, });
