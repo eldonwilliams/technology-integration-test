0673ff0abb1d294e5259f19c016500a6930119eb
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-17 11:24:24 -0600

    🦺 for usernames and pw & :memo:

--- server/src/services/accountService.ts
+++ server/src/services/accountService.ts
@@ -1,5 +1,7 @@
 import { redisClient, RedisError } from "../redisClient";
-import { LoggedInErrResponse } from "../util/authMiddleware";
+import { JWT_KEY, LoggedInErrResponse } from "../util/authMiddleware";
+import passwordValidator from "password-validator";
+import jwt from "jsonwebtoken";
 
 interface SuccessfulSignupResponse {
     success: true,
@@ -9,7 +11,7 @@ export interface SignupAlreadyExistsErr {
     err: "Already Exists",
 }
 
-export type SignupResponse = SuccessfulSignupResponse | SignupAlreadyExistsErr | LoggedInErrResponse | RedisError;
+export type SignupResponse = SuccessfulSignupResponse | SignupAlreadyExistsErr | AccDetailInvalidErr | LoggedInErrResponse | RedisError;
 
 export interface SignupBody {
     username: string,
@@ -29,7 +31,7 @@ function isAccount(acc: any): acc is Account {
     );
 }
 
-export const getAccountByUsername = (username: string): Promise<Account | undefined> => new Promise((resolve, reject) => {
+export const getAccountByUsername = (username: string): Promise<Account | undefined> => new Promise((resolve, _reject) => {
     redisClient.json.get(username)
         .then((found) => {
             if (!isAccount(found)) return resolve(undefined);
@@ -41,14 +43,53 @@ export const getAccountByUsername = (username: string): Promise<Account | undefi
         .catch(() => resolve(undefined));
 });
 
-export const setAccountByName = (username: string, newAccData: Account): Promise<boolean> => new Promise((resolve, reject) => {
+export const getAccountFromToken = (token: string): Promise<Account | undefined> => new Promise((resolve, _reject) => {
+    jwt.verify(token, JWT_KEY, {}, async (err, decoded) => {
+        if (err !== null || decoded === undefined || typeof decoded === "string") return resolve(undefined);
+        resolve(await getAccountByUsername(decoded.user))
+    });
+});
+
+export const setAccountByName = (username: string, newAccData: Account): Promise<boolean> => new Promise((resolve, _reject) => {
     redisClient.json.set(username, "$", JSON.stringify(newAccData))
         .then((ok) => resolve(ok === "OK"))
         .catch(() => resolve(false));
 });
 
-export const updateAccountByName = (username: string, path: keyof Account & string, newData: string): Promise<boolean> => new Promise((resolve, reject) => {
+export const updateAccountByName = (username: string, path: keyof Account & string, newData: string): Promise<boolean> => new Promise((resolve, _reject) => {
     redisClient.json.set(username, path, JSON.stringify(newData))
         .then((ok) => resolve(ok === "OK"))
         .catch(() => resolve(false));
 });
+
+export const genJWTForAccount = (account: Account): string => jwt.sign({ user: account.username, }, JWT_KEY);
+
+/**
+ * AccDetailInvalidErr and give status code 400
+ */
+export interface AccDetailInvalidErr {
+    err: "Validation Err",
+    details: { validation: string, message: string, argument?: number, }[],
+}
+
+/**
+ * Validates that a username follows the schema.
+ * If not it should throw AccDetailInvalidErr and give status code 400
+ */
+export const accountPasswordSchema = new passwordValidator()
+.is().min(8)
+.is().max(100)
+.has().uppercase()
+.has().lowercase()
+.has().not().spaces();
+
+/**
+ * Validates that a username follows the schema.
+ * If not it should throw AccDetailInvalidErr and give status code 400
+ */
+export const accountUsernameSchema = new passwordValidator()
+.is().min(4)
+.is().max(15)
+.has().not().uppercase()
+.has().not().symbols()
+.has().not().spaces();
