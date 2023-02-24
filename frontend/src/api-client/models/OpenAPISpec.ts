9a6759603f881548680cf797c63fc1d5a3f65972
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-24 09:24:18 -0600

    :fire: get out of here openapi-typescript-codegen!

--- frontend/src/api-client/models/OpenAPISpec.ts
+++
@@ -1,7 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-import type { ReturnType_typeofJSON_parse_ } from './ReturnType_typeofJSON_parse_';
-
-export type OpenAPISpec = ReturnType_typeofJSON_parse_;
