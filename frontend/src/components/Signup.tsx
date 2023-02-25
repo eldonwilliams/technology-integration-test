68defc90f0bd9898a9a2a386402add0f7bd8c1c7
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-24 20:29:30 -0600

    :children_crossing: add noConfirm to username & 🏗️ move to new system

--- frontend/src/components/Signup.tsx
+++ frontend/src/components/Signup.tsx
@@ -1,5 +1,6 @@
 import { HTMLInputTypeAttribute, useState } from "react";
 import { Link } from "react-router-dom";
+import useApi from "../hooks/apiHooks/useApi";
 
 
 type InputEventHandler = React.ChangeEventHandler<HTMLInputElement>;
@@ -9,22 +10,26 @@ interface SignupInputProps {
     name: string,
     state: Record<string, string>,
     eventFactory: (key: string) => InputEventHandler,
+    noConfirm?: boolean,
 }
 
-function SignupInput({ type, name, state, eventFactory, }: SignupInputProps) {
+function SignupInput({ type, name, state, eventFactory, noConfirm = false }: SignupInputProps) {
     const capitalized = name.slice(0,1).toUpperCase() + name.toLowerCase().slice(1);
     const lowercase = name.toLowerCase();
 
     return (
         <>
             <input type={type} placeholder={capitalized} value={state[lowercase] ?? ""} onChange={eventFactory(lowercase)} />
-            <input type={type} placeholder={`Confirm ${capitalized}`} value={state[`confirm${capitalized}`] ?? ""} onChange={eventFactory(`confirm${capitalized}`)}/>
+            {!noConfirm && <input type={type} placeholder={`Confirm ${capitalized}`} value={state[`confirm${capitalized}`] ?? ""} onChange={eventFactory(`confirm${capitalized}`)}/>}
         </>
     );
 }
 
 function Signup() {
     const [formState, setFormState] = useState<Record<string, string>>({});
+    const api = useApi();
+
+    const signupRequest = api.path('/account/signup').method('post').create();
 
     const getFormUpdater = (key: string): InputEventHandler => {
         return (event) => setFormState((old) => ({ ...old, [key]: event.target.value, }));
@@ -32,25 +37,40 @@ function Signup() {
 
     const submit = () => {
         if (
-            (formState.username !== formState.confirmUsername) ||
             (formState.password !== formState.confirmPassword) ||
-            (Object.keys(formState).length !== 4)
+            (Object.keys(formState).length !== 3)
         ) return;
 
-        setFormState((old) => ({ ...old, pending: "true", }));
+        setFormState({ pending: "true" });
 
-        fetch(`${import.meta.env.VITE_API_URL}/auth/signup`, {
-            body: JSON.stringify({
-                username: formState.username,
-                password: formState.password,
-            }),
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(res => setFormState((old) => ({ ...old, successState: res.status === 200 ? "Successfully created account!" : "Error", })))
-            .catch(err => setFormState((old) => ({ ...old, successState: "Bad Error", })))
+        signupRequest({ username: formState.username, password: formState.password, })
+            .then((response) => {
+                if (response.status !== 201) return;
+                setFormState({ successState: "Successfully created account!", });
+            })
+            .catch((e) => {
+                if (e instanceof signupRequest.Error) {
+                    const error = e.getActualType();
+                    let successState = "An Unknown Error Occurred...";
+                    switch (error.status) {
+                        case 400:
+                            let badUser = error.data.details.username.length !== 0;
+                            let badPass = error.data.details.password.length !== 0;
+                            successState = `The ${badUser ? "username" : ''}${badUser && badPass ? " and " : ""}${badPass ? "password" : ''} was invalid`
+                            break;
+                        case 403:
+                            successState = "You may not register an account whilst logged in";
+                            break;
+                        case 409:
+                            successState = "An account with that username already exists!";
+                            break;
+                        case 500:
+                            successState = "There was an internal server error :(";
+                            break;
+                    }
+                    setFormState({ successState, })
+                }
+            });
     }
 
     if (formState?.successState)
@@ -64,7 +84,7 @@ function Signup() {
     return (
         <>
             <form>
-                <SignupInput type="text" name="Username" state={formState} eventFactory={getFormUpdater} />
+                <SignupInput type="text" name="Username" state={formState} eventFactory={getFormUpdater} noConfirm />
                 <SignupInput type="password" name="Password" state={formState} eventFactory={getFormUpdater} />
             </form>
             <div className="links">
