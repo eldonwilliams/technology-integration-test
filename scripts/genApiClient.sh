acb967f8ecd4cc9806a4ad6f99fc357e85c2e2ee
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-25 13:23:56 -0600

    :hammer: added apiTypes for testing

--- scripts/genApiClient.sh
+++ scripts/genApiClient.sh
@@ -2,5 +2,6 @@
 directory=$(pwd)
 cd ../server
 tsoa spec-and-routes
+npx openapi-typescript ./src/tsoa/swagger.json --output ./spec/api/apiTypes.ts
 cd $directory
 npx openapi-typescript ../server/src/tsoa/swagger.json --output ./src/apiTypes.ts
