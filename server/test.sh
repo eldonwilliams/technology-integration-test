8c51373b925f4e5d0cead1c2b8cdd2bd7374778c
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-27 08:32:44 -0600

    :hammer: script for test

---
+++ server/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is a small runner for the Docker test stage.
+# Do not run this script
+
+if [ -f /.dockerenv ]; then
+    # Why doesn't pm2 start work on a Docker run command :(
+    pm2 start ./dist/index.js --no-autorestart
+    jest --no-color 2>./jest-reports/jest.report.txt
+else
+    echo You\'re not Docker!
+    exit 1
+fi
