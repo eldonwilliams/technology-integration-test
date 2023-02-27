481e6acb3ac8b0330e158ce6efa7576dca91219c
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-27 08:33:10 -0600

    :fire: no more "test gen"

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,8 @@
 import { getEnvNumber } from "./util/getEnv";
 import app from "./server";
-import { generateTests } from "./testGeneration";
 
 const REST_PORT = getEnvNumber("REST_PORT", 8000) as number;
 const SOCKET_PORT = getEnvNumber("SOCKET_PORT", 7979) as number;
-
-generateTests();
 
 app.listen(REST_PORT, () => {
     console.log(`Ready on port: ${REST_PORT}`);
