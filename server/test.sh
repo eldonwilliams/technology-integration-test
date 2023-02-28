8ff1d1e227583ca9a38543b6845380f7114bf4d4
Eldon Williams <eldon.ew.com@gmail.com>
2023-02-28 14:36:08 -0600

    🔊 made pm2 output logs into jest folder
    These logs will not be cleared between runs. You will need to clear them manually.

--- server/test.sh
+++ server/test.sh
@@ -5,7 +5,8 @@
 
 if [ -f /.dockerenv ]; then
     # Why doesn't pm2 start work on a Docker run command :(
-    pm2 start ./dist/index.js --no-autorestart
+    pm2 start ./dist/index.js --no-autorestart -o ./jest-reports/server-logs.report.txt -e ./jest-reports/server-crash.report.txt
+    sleep 5
     jest --no-color 2>./jest-reports/jest.report.txt
 else
     echo You\'re not Docker!
