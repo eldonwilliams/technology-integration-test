3a8c8222b782fc432be51520d8f0f19777121a91
Eldon Williams <eldon.ew.com@gmail.com>
2023-03-10 08:22:03 -0600

    :sparkles: upd imports and make tests better

--- server/spec/api/status.spec.ts
+++ server/spec/api/status.spec.ts
@@ -1,14 +1,39 @@
-import { describe, expect, test } from "@jest/globals"
-import testApiFetcher from "./api";
+import { beforeAll, describe, expect, test } from "@jest/globals";
+import testApiFetcher, { getTestSessionToken } from "./mock/api";
 
 describe("status api tests", () => {
     const statusRequest = testApiFetcher.path("/status").method("get").create();
+    let sessionToken: string = "";
+
+    beforeAll(async () => {
+        let got = await getTestSessionToken();
+        if (got === undefined) throw new Error("SessionToken was undefined, error occurred whilst logging in");
+        sessionToken = got;
+    });
 
     test("getting status while not authed works", () => {
-        statusRequest({}, {}).then((value) => {
-            expect(value.status).toBe(200);
-            expect(value.data.okay).toBe(true);
-            expect(value.data.authenticated).toBe(false);
+        statusRequest({}, {
+            headers: {
+                "Cookie": ``,
+            }
+        }).then((response) => {
+            expect(response.status).toBe(200);
+            expect(response.data.okay).toBe(true);
+            expect(response.data.authenticated).toBe(false);
+        }).catch((e) => {
+            throw new Error(e.toString());
+        });
+    });
+
+    test("getting status while authenticated works", () => {
+        statusRequest({}, {
+            headers: {
+                "Cookie": `sessionToken=${sessionToken};`
+            }
+        }).then((response) => {
+            expect(response.status).toBe(200);
+            expect(response.data.okay).toBe(true);
+            expect(response.data.authenticated).toBe(true);
         }).catch((e) => {
             throw new Error(e.toString());
         });
